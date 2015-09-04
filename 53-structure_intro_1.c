f82532a09ed7815623ca02af28643ab48de27216
Harish SK <hsk@outlook.com>
2015-09-05 03:34:50 +0530

    Fixed syntax error and foramted it correctly

--- 53-structure_intro_1.c
+++ 53-structure_intro_1.c
@@ -2,14 +2,15 @@
 
 int main()
 {
-   char name[5][20] = {"Ram", "Mason", "Akbar", "Emma", David"};
-	int  age[5] = {7, 9, 5, 10, 14 };
-	int  class[5] = {2, 4, 1, 5, 9};
-	
-	int i = 0;
+   char name[5][20] = {"Ram", "Mason", "Akbar", "Emma", "David"};
+   int  age[5] = {7, 9, 5, 10, 14 };
+   int  class[5] = {2, 4, 1, 5, 9};
+   
+   int i = 0;
 
-	for(i = 0; i < 5; i++) {
-	    printf("%s is %d years old and studies in %d Class\n", name[i], age[i], class[i]);
+   for(i = 0; i < 5; i++) {
+		printf("%s is %d years old and studies in %d Class\n",
+				 name[i], age[i], class[i]);
    }
-	return 0;
+   return 0;
 }
