a4597ef6d1a2fb658f756f403c6728c28ba807fd
Harish SK <harish-sk@users.noreply.github.com>
2015-09-05 11:21:55 +0530

    Create 53_structure_intro_1.c

---
+++ 53_structure_intro_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+   char name[5][20] = {"Ram", "Mason", "Akbar", "Emma", David"};
+	int  age[5] = {7, 9, 5, 10, 14 };
+	int  class[5] = {2, 4, 1, 5, 9};
+	
+	int i = 0;
+
+	for(i = 0; i < 5; i++) {
+	    printf("%s is %d years old and studies in %d Class\n", name[i], age[i], class[i]);
+   }
+	return 0;
+}
